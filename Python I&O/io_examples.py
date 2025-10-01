cbedb1e62ae1af7bedef75e4d1609a54b15888f6
Zainabfatimahh <xainabfatimahh@gmail.com>
2025-10-01 04:51:04 -0700

    Create io_examples.py

---
+++ Python I&O/io_examples.py
@@ -0,0 +1,20 @@
+#Printing text
+print ('Hello World')
+#Printing numbers
+print (13)
+
+#Printing multiple things (Text or number)
+print("My age is",20)
+#(,) is used as space in python language
+
+#using f-stream for formatted output
+name="Alice"
+age=20
+print(f"My name is {name} and my age is {age}")
+
+#To move the output on the next line:
+print("Hello \nWorld")
+
+#taking name as input
+name=(input"Enter your name:")
+print(hello, name)
