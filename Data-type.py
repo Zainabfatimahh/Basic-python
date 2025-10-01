e1c58062fb41bc11419b02a19a1c0986af16b25a
Zainabfatimahh <xainabfatimahh@gmail.com>
2025-10-01 09:03:43 -0700

    Create Data-type.py

---
+++ Data-type.py
@@ -0,0 +1,9 @@
+'''A data type tells Python what kind of value you're working with — for example, a number, text, list, etc.
+int for (1,2,3,4)
+string for ("Ayesha")
+float for ( 2.0,9.1)
+complex for e.g. x = 2 + 3j'''
+#For e.g,
+x=20
+print(type(x))  # Output: <class 'int'>
+print(type(x)) # is used for checking the data type of the variable
